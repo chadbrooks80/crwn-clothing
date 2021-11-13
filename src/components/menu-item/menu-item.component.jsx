f1e0c7ed7590020a6c04abb738e5744819015179
Chad <chad@taskflowaccounting.com>
2021-11-13 20:04:50 +0800

    broken our homepage into smaller componeents, finished styling our menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,15 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size}) => (
     // we can add styles directly on the jsx element
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }}
-    className= {`${size} menu-item`} >
+    <div className= {`${size} menu-item`} >
+        <div 
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            className="background-image" 
+        />
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
