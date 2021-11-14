42df8cfde9da4a05ec8282d1579fb21a106c23ad
Chad <chad@taskflowaccounting.com>
2021-11-14 12:01:17 +0800

    added routing into our pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import HomePage from './pages/homepage/homepage.compoenent';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+const HatPage = () => (
+  <h1>HAT PAGE</h1>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />      
+        <Route exact path='/hats' component={HatPage} />      
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import {withRouter} from 'react-router-dom';
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, imageUrl, size}) => (
-    // we can add styles directly on the jsx element
-    <div className= {`${size} menu-item`} >
-        <div 
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-            className="background-image" 
-        />
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
+    return (
+        // we can add styles directly on the jsx element
+        <div className= {`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
+            <div 
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+                className="background-image" 
+            />
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
-    </div>
-)
-
-export default MenuItem
+    )
+}
+export default withRouter(MenuItem)
