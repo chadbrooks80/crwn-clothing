41f546cc3c109b1b8d0502d323634e4c8f406521
Chad <chadbrooks80@gmail.com>
2021-11-15 20:58:02 +0800

    added the sign up field

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './pages/homepage/homepage.compoenent';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 import './App.css';
 import React from 'react';
@@ -20,10 +20,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        createUserProfileDocument(userAuth).then(userRef => {
+        
+          userRef.onSnapshot(snapShot => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            }, () => console.log(this.state))
+          })
 
-      console.log(user)
+          console.log(this.state)
+
+        })
+
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
