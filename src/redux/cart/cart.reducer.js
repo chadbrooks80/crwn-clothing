0c8908e76647c7cc4024f5a4e69b00d02382230c
Chad Brooks <chad@boostmediagroup.com>
2021-11-18 11:28:51 +0800

    added in remove item from cart feature on the cart page

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -9,7 +9,6 @@ const INITIAL_STATE = {
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case CartActionTypes.TOOGLE_CART_HIDDEN:
-            console.log("cart hidden button")
             return {
                 ...state,
                 hidden: !state.hidden
@@ -19,6 +18,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
             }
 
         default:
