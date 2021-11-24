31095ce8efe8b927f402f1ab5eec27a2438055a1
Chad Brooks <chad@boostmediagroup.com>
2021-11-24 10:40:14 +0800

    added shop data from firebase to redux instead of on shop component w/ redux thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,66 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import {Route} from 'react-router-dom'
 
-import { updateCollections } from "../../redux/shop/shops.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shops.actions";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionPage from "../collection/collection.component";
 
-import {firestore, convertCollectionSnapshopToMap} from '../../firebase/firebase.utils'
-
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
 const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
-    state = {
-        //this is used to tell if we are still loading the collections from firebase.
-        // if not true it will display teh spinner until data has been downloaded it will change to false. 
-        loading: true
-    }
     
-    
-    unsubscribeFromSnapshop = null
-    
-    // connect to firebase database and get collections 
     componentDidMount() {
-        
-        // creates Firebase reference for the collection called collections (these are items can shop for)
-        const collectionRef = firestore.collection('collections')
-
-        // fetch example but not using but another way to get snapshop
-        /* 
-        fetch('https://firestore.googleapis.com/v1/projects/crwn-db-b07b9/databases/(default)/documents/collections')
-        .then(response => response.json())
-        .then(collections => console.log("thecollections", collections))
-        */
-
-        // takes referene and gets snapshot
-        collectionRef.get().then(snapshot => {
-            // this app converts the snapshot data into the proper object to place into starte
-            const collectionsMap = convertCollectionSnapshopToMap(snapshot)
-            this.props.updateCollections(collectionsMap) //dispatch into redux store
-            this.setState({loading: false}) //changes loading to false because the redux store has been updated (see the routes below, which reference this state.) 
-        })        
-        
+        const {fetchCollectionsStartAsync} = this.props
+        fetchCollectionsStartAsync()
     }
     
     render() {
-            const {match} = this.props
-            const {loading} = this.state
+            const {match, isCollectionFetching} = this.props
             return(
             <div className="shop-page">
                 {/* collections overview and colltion page both functions that return the spinner until loading is false. */}
-                < Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
-                < Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+                < Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />} />
+                < Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />} />
             </div>
         )
     }
 } 
 
-const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+
+const mapStateToProps = createStructuredSelector({
+    isCollectionFetching: selectIsCollectionFetching
 })
 
-export default connect(null, mapDispatchToProps)(ShopPage)
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStartAsync: ()  => dispatch(fetchCollectionsStartAsync())
+})
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage)
--- src/redux/shop/shops.actions.js
+++ src/redux/shop/shops.actions.js
@@ -1,8 +1,34 @@
 import ShopActionTypes from "./shop.types";
 
-export const updateCollections = collectionsMap => {
+import { firestore, convertCollectionSnapshopToMap } from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = () => {
     return {
-        type: ShopActionTypes.UPDATE_COLLECTIONS,
-        payload: collectionsMap
+        type: ShopActionTypes.FETCH_COLLECTIONS_START
     }
 }
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = (errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+}))
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        // creates Firebase reference for the collection called collections (these are items can shop for)
+        const collectionRef = firestore.collection('collections')
+        dispatch(fetchCollectionsStart())
+
+        // takes referene and gets snapshot
+        collectionRef.get().then(snapshot => {
+            // this app converts the snapshot data into the proper object to place into starte
+            const collectionsMap = convertCollectionSnapshopToMap(snapshot)
+            dispatch(fetchCollectionsSuccess(collectionsMap)) //dispatch into redux store
+        }).catch(error => dispatch(fetchCollectionsFailure(error.msg)))
+    }
+} 
